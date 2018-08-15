f89f66271171047210e86ca1c42492e64f50626f
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-14 17:55:02 -0700

    changed code renders

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+
+  state = {
+    content: 'Whooohoooo',
+  };
+
+  handleContentChange = (content = ' ') => {
+    this.setState({ content });
+  };
+
+
+  render() {
+    const { content } = this.state;
+
+    return (
+      <main className={StyleSheet.app}>
+        <section>
+          <h2>Set Options</h2>
+          <Content content={content} onChange={this.handleContentChange}/>
+          
+        </section>
+      </main>
+    );
+  }
+}
+
+function Content({ content, onChange }) {
+  return (
+    <p>
+      <label>
+        Content: 
+        <input 
+          value={content} 
+          onChange={({ target }) => onChange(target.value)}
+        />
+      </label>
+    </p>
+  );
+}
+
+export default App;
