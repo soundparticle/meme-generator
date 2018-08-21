3f92bf97b56a87d3b03029baf1247fff2b4e32f9
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-20 21:15:30 -0700

    color select almost all working

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import dom2image from 'dom-to-image';
 import fileSaver from 'file-saver';
 import styles from './App.css';
@@ -8,11 +8,20 @@ class App extends Component {
   
   state = {
     content: 'Take me to campsite #7!',
+    memeHeader: 'Take me to campsite #666',
+    color: '#ffffff',
+    textSize: '6',
     url:'http://www.vaguebuttrue.com/images/1435713417-RaccoonridesalligatorWEBSITE.jpg' 
   };
-  
+
+  handleHeaderChange = ({ target }) => {
+    this.setState({
+      memeHeader: target.value
+    });
+  };
+
   handleContentChange = (content = ' ') => {
-    this.setState({ content });
+    this.setState({ content }); 
   };
   
   handleBackgroundChoose = (url = '') => {
@@ -20,45 +29,55 @@ class App extends Component {
   };
   
   handleExport = () => {
+    // const meme = document.getElementById('meme');
     dom2image.toBlob(this.image)
       .then(blob => {
-        fileSaver.saveAs(blob, 'cute-cowsay.png');
+        fileSaver.saveAs(blob, 'my-meme.png');
       });
+  };
+
+  handleColorChange = ({ target }) => {
+    this.setState({ color: target.value });
+  };
+  handleTextChange = ({ target }) => {
+    this.setState({ textSize: target.value });
   };
   
   
   render() {
-    const { content, url } = this.state;
+    const { content, url, memeHeader, color, textSize } = this.state;
 
     return (
       <main className={styles.app}>
         <section>
           <h1>Set Options</h1>
-          <Content content={content} onChange={this.handleContentChange}/>
           <Background url={url} onChoose={this.handleBackgroundChoose}/>
+          <Content content={content} onChange={this.handleContentChange} memeHeader={memeHeader}/>
+          {/* <Meme memeHeader={memeHeader} url={url} color={color} textSize={textSize}/> */}
+          <label>Choose Font Color:<input type="color" value={color} onChange={this.handleColorChange}/></label>
           <p>
             <button onClick={this.handleExport}>Export</button>
           </p>
         </section> 
 
         <section className="cow-say">
-          <h2>Memes Of The Imagination</h2>
+          <h2 style={{ color: color }}>Memes Of The Imagination</h2>
+          <Meme content={content} url={url} memeHeader={memeHeader} onChange={this.handleColorChange} color={color} textSize={textSize}/>
           <p ref={node => this.image = node}>
-            <Meme content={content} url={url}/>
           </p>
         </section>
-
       </main>
     );
   }
 }
 
-function Meme({ content, url }) {
+function Meme({ content, url, memeHeader, color, textSize }) {
 
   return (
-    <Fragment>
-      <pre style={{ background: `url(${url}) no-repeat ` }}>{content}</pre> 
-    </Fragment>
+    <div>
+      <h1 id="meme-header" style={{ color: color }}><font size={textSize}>{memeHeader}</font></h1> 
+      <pre id="meme" className="meme-container" style={{ background: `url(${url}) no-repeat ` }}>{content}</pre>
+    </div>
   );
 }
 
@@ -79,7 +98,7 @@ function Background({ url, onChoose }) {
 function Content({ content, onChange }) {
   return (
     <p>
-      <label>
+      <label >
         Content: 
         <input 
           value={content} 
@@ -87,7 +106,7 @@ function Content({ content, onChange }) {
         />
       </label>
     </p>
-  );
+  );  
 }
 
 export default App;
